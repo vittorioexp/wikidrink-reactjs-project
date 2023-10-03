a0bffc0dd676aed2112e52c5ee974af70dba39d6
Vittorio <vi.esposito@reply.it>
2023-10-03 11:24:11 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import { Navbar, Sidebar, Footer } from "./components";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screen/HomeScreen";
 import AboutScreen from "./screen/AboutScreen";
 import ContactScreen from "./screen/ContactScreen";
 import SingleCocktailScreen from "./screen/SingleCocktailScreen";
 import ErrorScreen from "./screen/ErrorScreen";
+import { useGlobalContext } from "./context";
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/about" element={<AboutScreen />} />
+        <Route path="/contact" element={<ContactScreen />} />
+        <Route path="/cocktail/:id" element={<SingleCocktailScreen />} />
+        <Route path="*" element={<ErrorScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
