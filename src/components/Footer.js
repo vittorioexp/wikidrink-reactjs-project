0782672db83dcbec5e47dcc569b924242c470222
Vittorio <vi.esposito@reply.it>
2023-10-05 10:32:49 +0200

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="container footer-container">
+        <h4 className="footer-text">
+          all &copy; reserved to
+          <span className="brand-color"> drink team</span>
+        </h4>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
