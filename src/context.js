b7ea492f20a1b1fbd56cf7105a4bf692689e196e
Vittorio <vi.esposito@reply.it>
2023-10-05 10:31:44 +0200

    add sidebar to context.js

--- src/context.js
+++ src/context.js
@@ -3,7 +3,21 @@ import { useState, useContext, createContext } from "react";
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="prova">{children}</AppContext.Provider>;
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  return (
+    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 const useGlobalContext = () => {
