2e066b372680edb5a87c287d9910076b28e10e26
Vittorio <vi.esposito@reply.it>
2023-10-09 09:35:51 +0200

    fix context.js

--- src/context.js
+++ src/context.js
@@ -1,9 +1,13 @@
 import { useState, useContext, createContext } from "react";
+import useFetch from "./useFetch";
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [query, setQuery] = useState("negroni");
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const { isLoading, isError, data, count } = useFetch(`s=${query}`);
 
   const openSidebar = () => {
     setIsSidebarOpen(true);
@@ -13,8 +17,35 @@ const AppProvider = ({ children }) => {
     setIsSidebarOpen(false);
   };
 
+  const searchCocktail = (input) => {
+    setQuery(input);
+  };
+
+  const getScrollPosition = (value) => {
+    setScrollPosition(value);
+  };
+
+  const deleteScrollPosition = (value) => {
+    setScrollPosition(0);
+  };
+
   return (
-    <AppContext.Provider value={{ isSidebarOpen, openSidebar, closeSidebar }}>
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        openSidebar,
+        closeSidebar,
+        query,
+        isLoading,
+        isError,
+        data,
+        count,
+        searchCocktail,
+        scrollPosition,
+        getScrollPosition,
+        deleteScrollPosition,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
